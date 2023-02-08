b9a42765f4e80f82a7cceba0bf2e069fecb45c57
전지훈 <junji636@gmail.com>
2023-02-08 23:17:24 +0900

    realm データ追加

--- SwiftUI_Realm/SwiftUI_Realm/Manager/RealmManager.swift
+++ SwiftUI_Realm/SwiftUI_Realm/Manager/RealmManager.swift
@@ -52,4 +52,5 @@ class RealmManager: ObservableObject {
             }
         }
     }
+    
 }
--- SwiftUI_Realm/SwiftUI_Realm/View/JsonView.swift
+++ SwiftUI_Realm/SwiftUI_Realm/View/JsonView.swift
@@ -11,7 +11,11 @@ struct JsonView: View {
     
     var body: some View {
         ZStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            List {
+                ForEach(realmManager.resultList, id: \.self) { data in
+                    Text("\(data.id)")
+                }
+            }
         }
         .onAppear {
             readFromJsonFile()
